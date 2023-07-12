8d734c8a3d660b541a1420eb8f22aa7815162ba4
Pedro Von Anht <123915039+pedrovonanht@users.noreply.github.com>
2023-07-12 18:02:13 -0300

    mecajato aula 2

--- templates/static/clientes/js/clientes.js
+++ templates/static/clientes/js/clientes.js
@@ -23,19 +23,46 @@ function mascara1(i){
    
     var v = i.value;
     
-    if(isNaN(v[v.length-1] || v == )){ // impede entrar outro caractere que não seja número
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
        i.value = v.substring(0, v.length-1);
        return;
     }
     
     i.setAttribute("maxlength", "14");
-    if (v.length == 3 || v.length == 7) i.value += "."; 
-    if (v.length == 4) {
-        i.value = v.substring(0,3) + '.' + v.substring(3,4)}
-    if (v.length == 8) {
-        i.value = v.substring(0,7) + '.' + v.substring(7,8)}
-    if (v.length == 12) {
-            i.value = v.substring(0,11) + '-' + v.substring(11,12)}
-    if (v.length == 11) i.value += "-";
+    switch(){ 
+    case v.length == 3 || v.length == 7):
+         i.value += "."
+         break
     
+    case v.length == 4: 
+        i.value = v.substring(0,3) + '.' + v.substring(3,4)
+        break
+
+    case v.length == 8:
+        i.value = v.substring(0,7) + '.' + v.substring(7,8)
+        break
+    
+    case v.length == 12:
+        i.value = v.substring(0,11) + '-' + v.substring(11,12)
+        break
+    
+    case v.length == 11:
+      i.value += "-"
+      break
+    }
+}
+
+function exibir_form(tipo){
+    
+    att_cliente = document.getElementById('atualizar_cliente')
+    add_cliente = document.getElementById('adicionar_cliente')
+
+    if (tipo == 1){
+        att_cliente.style.display = 'none'
+        add_cliente.style.display = 'block'
+    }
+    else if (tipo == 2) {
+        att_cliente.style.display = 'block'
+        add_cliente.style.displat = 'none'
+    }
 }
